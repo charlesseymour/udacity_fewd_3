c90c2038eef9f427627e134827ec86188d10e688
Charles Seymour <charlesseymour@yahoo.com>
2020-03-13 10:04:59 -0500

    Removed superfluous files

---
+++ website/app.js
@@ -0,0 +1,10 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// OpenWeatherMap API Key
+
+const key = b9186922d7e27088984892c4cedcd3d6;
+
